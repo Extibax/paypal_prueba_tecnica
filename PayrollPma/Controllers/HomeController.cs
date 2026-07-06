15d8910a63ab08a046a570d97ce5dfa55dcb29fb
Bedoya Dev <extibax@gmail.com>
2026-07-06 16:55:17 -0500

    refactor(routing): arrancar en tabla empleados
    - Program.cs: ruta default {controller=Employees} (era Home)
    - HomeController.Index(): RedirectToAction a Employees/Index como
      salvaguarda para quien llegue a /Home/Index
    - Elimina Views/Home/Index.cshtml (landing useless ya no necesaria)
    - Brand link en navbar: asp-controller="Employees" para evitar
      redirect intermedio

--- PayrollPma/Controllers/HomeController.cs
+++ PayrollPma/Controllers/HomeController.cs
@@ -13,10 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+    public IActionResult Index() => RedirectToAction("Index", "Employees");
 
     public IActionResult Privacy()
     {
